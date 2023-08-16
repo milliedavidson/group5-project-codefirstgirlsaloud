ac3af1831f70c3e5892e377254f08bb190b2dd56
lucytesco <119863442+lucytesco@users.noreply.github.com>
2023-08-16 18:09:47 +0100

    Updated route functions to include Ella's new API code, having issues pulling the data through to the browser as classes are not serializable in json so looking into that. This may be redundant once we have coded the html for the results page anyway.

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
-from ella_code_draft import get_books_by_subject
+from main import fetch_books, format_and_print_books
+from input import format_category_for_search
 # from draft_config import MY_API_KEY
 
 app = Flask(__name__)
@@ -11,21 +12,31 @@ def home():
         return file.read()
 
 
-@app.route('/process', methods=['POST'])
-def process():
-    # selected_genre = request.form['genres']
-    selected_category = request.form['categories']
+@app.route('/results', methods=['POST'])
+def process_user_input():
+    # selected_genre = request.form['genres'] -- Not sure if this is needed at all, though keeping here with regards
+    # to Olivia's comment about excluding fiction from non-fiction searches
+
+    selected_category = format_category_for_search(request.form['categories'])
     selected_book_length = request.form['book_length']
     selected_min_published_date = int(request.form['year_published_min'])
     selected_max_published_date = int(request.form['year_published_max'])
+    selected_order_by = request.form['order_by']
+
     # You can now use the 'selected_' values in your Python code
+    # This is just for internal purposes to see what has come through as user input criteria
     print(f'''
     Category: {selected_category}
     Book Length: {selected_book_length}
     Published Between: {selected_min_published_date} - {selected_max_published_date}
+    Order By: {selected_order_by}
 ''')
-    results = get_books_by_subject(selected_category, selected_book_length, selected_min_published_date, selected_max_published_date)
-    return jsonify(results)
+
+    books = fetch_books(selected_category, selected_book_length, selected_min_published_date, selected_max_published_date, selected_order_by)
+
+    # results = format_and_print_books(books)
+
+    return jsonify(books)
 
 
 if __name__ == '__main__':

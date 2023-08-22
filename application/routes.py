69109a9c661f2c80ab397ff0693368841a577bae
lucytesco <119863442+lucytesco@users.noreply.github.com>
2023-08-22 19:49:45 +0100

    Reformats

--- application/routes.py
+++ application/routes.py
@@ -6,13 +6,13 @@ from db.functions import (
     format_book_categories,
     format_book_rating,
     format_book_length,
-    format_category_for_search
+    format_category_for_search,
 )
 
-app.add_template_global(format_book_published, 'format_book_published')
-app.add_template_global(format_book_categories, 'format_book_categories')
-app.add_template_global(format_book_rating, 'format_book_rating')
-app.add_template_global(format_book_length, 'format_book_length')
+app.add_template_global(format_book_published, "format_book_published")
+app.add_template_global(format_book_categories, "format_book_categories")
+app.add_template_global(format_book_rating, "format_book_rating")
+app.add_template_global(format_book_length, "format_book_length")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -20,7 +20,9 @@ def home():
     if request.method == "POST":
         selected_genre = request.form.get("selected_genre")
         selected_category = request.form.get("categories")
-        formatted_category = format_category_for_search(selected_category, selected_genre)
+        formatted_category = format_category_for_search(
+            selected_category, selected_genre
+        )
         selected_book_length = request.form["book_length"]
         selected_min_published_date = int(request.form["year_published_min"])
         selected_max_published_date = int(request.form["year_published_max"])
@@ -39,13 +41,13 @@ def home():
             selected_book_length,
             selected_min_published_date,
             selected_max_published_date,
-            selected_order_by
+            selected_order_by,
         )
-        return render_template('results.html', books=books)
+        return render_template("results.html", books=books)
     else:
-        return render_template('home.html')
+        return render_template("home.html")
 
 
 @app.errorhandler(404)
 def page_not_found():
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404

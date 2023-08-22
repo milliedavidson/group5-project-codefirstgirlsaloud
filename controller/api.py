c49236df6e85f4d6d06a880b0ef9c588b7a1d6eb
Millie Davidson <milliedavidson@hotmail.com>
2023-08-22 20:28:40 +0100

    Error handling added to api code

--- controller/api.py
+++ controller/api.py
@@ -1,10 +1,62 @@
+"""
+Module to interact with the Google Books API.
+"""
+
+import logging
 import requests
 
 
-# Function to call API endpoint
-def call_api(subject, page, order_by):
-    endpoint = f"https://www.googleapis.com/books/v1/volumes?q={subject}&maxResults=40&printType=books&language=en&startIndex={page*40}&orderBy={order_by}"
-    response = requests.get(endpoint)
+class ApiError(Exception):
+    """
+    Exception raised when there is an error interacting with the API
+    """
+
+
+logger = logging.getLogger(__name__)
+
+
+def call_api(subject, page, order_by, timeout=10):
+    """
+    Call API endpoint and return results.
+
+    Parameters:
+    subject (str): Search term
+    page (int): Page number of results
+    order_by (str): Sort order
+    timeout (int): API request timeout in seconds
+
+    Raises:
+    ApiError: Custom exception for any API errors
+
+    Returns:
+    list: List of result items
+    """
+
+    if not isinstance(subject, str):
+        raise TypeError("Subject must be a string")
+
+    if not isinstance(page, int):
+        raise TypeError("Page must be an integer")
+
+    endpoint = (
+        f"https://www.googleapis.com/books/v1/volumes?q={subject}"
+        f"&maxResults=40&printType=books&language=en"
+        f"&startIndex={page*40}&orderBy={order_by}"
+    )
+
+    logger.info("Requesting URL: %s", endpoint)
+
+    try:
+        response = requests.get(endpoint, timeout=timeout)
+        response.raise_for_status()
+
+        logger.info("API request succeeded, status code %s", response.status_code)
+
+    except requests.exceptions.RequestException as error:
+        raise ApiError from error
+
+    if response.status_code == 404:
+        return []
+
     data = response.json()
-    print(endpoint)
     return data.get("items", [])

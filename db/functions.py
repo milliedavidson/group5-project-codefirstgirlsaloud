1ab7ba310f5a3e254d487ea24657f375dd659bc1
elenanewell <128835719+elenanewell@users.noreply.github.com>
2023-08-23 16:36:35 +0100

    Excluded_categories fixed for fiction

--- db/functions.py
+++ db/functions.py
@@ -5,14 +5,14 @@ from model.book import Book
 
 # MAIN BOOK FILTER FUNCTION
 # Fetches books based on user input and criteria
-def find_books(subject, book_length, start_year, end_year, min_results=10):
+def find_books(selected_genre, selected_category, book_length, start_year, end_year, min_results=10):
     results = []  # Initialise list to store results
     seen_books = set()  # Maintain a set of seen titles and authors so no duplicates
     page = 0
     empty_pages = 0
 
     while len(results) < min_results:
-        items = call_api(subject, page)  # Fetch books from API
+        items = call_api(selected_category, page)  # Fetch books from API
         if not items:
             empty_pages += 1
             if empty_pages == 5:  # When there have been 5 pages in a row with no results the loop ends
@@ -30,16 +30,18 @@ def find_books(subject, book_length, start_year, end_year, min_results=10):
                 if any(
                     value == "N/A" or value == 0 for value in book.__dict__.values()
                 ):
+                    print("MISSING VALUES")
                     continue
 
                 # 2nd filter check for duplicates to ensure unique books in the results
                 book_id = get_book_id(book)
                 if book_id in seen_books:
+                    print("ALREADY SEEN")
                     continue  # Skips over this book, as already been seen
                 seen_books.add(book_id)  # If not already seen, adds it to the list
 
                 # 3rd filter checks if there are any excluded categories and if rating is 4+
-                if excluded_categories(book) or subject not in book.categories:
+                if excluded_categories(book, selected_genre, selected_category):
                     continue
 
                 # 4th filter checks within date range and selected book length
@@ -51,6 +53,8 @@ def find_books(subject, book_length, start_year, end_year, min_results=10):
                 ):
                     # If the book gets through these filters it is added book to results
                     results.append(book)
+                else:
+                    print("WRONG LENGTH")
 
                     if len(results) == min_results:  # Stops when 10 are found
                         break
@@ -74,16 +78,23 @@ def get_book_id(book):
 
 # Checks the excluded words against those in the book's categories
 # If any are found, it returns the book so they can be filtered out in the find_books function
-def excluded_categories(book):
-    excluded = {"young adult", "juvenile"}
-    categories = book.categories.lower()
-    return book if any(substring in categories for substring in excluded) else None
+def excluded_categories(book, selected_genre, selected_category):
+    if selected_genre == "fiction":
+        if book.categories.lower() != selected_genre:
+            print("WRONG CATEGORY")
+            return book
+    else:  # if selected_genre == "non-fiction"
+        if book.categories.lower != selected_category.lower():
+            print("WRONG CATEGORY")
+            return book
 
 
-# # Checks the book's rating is less than 4
-# # If less than 4, it returns True so they can again be filtered out in the main.py
-# def low_rating(book):
-#     return book.average_rating < 4
+def format_category_for_search(category, selected_genre):
+    if selected_genre == "fiction":
+        formatted_category = "fiction+" + category
+    else:
+        formatted_category = category
+    return formatted_category
 
 
 # Uses the published date and returns it as just the year to compare against user's input year
@@ -125,11 +136,6 @@ def format_book_published(book):
     return f"{formatted_date(book)}\n"
 
 
-# Formats the categories for HTML
-def format_book_categories(book):
-    return book.categories
-
-
 # Formats the rating for HTML
 def format_book_rating(book):
     return f"{book.average_rating} stars"
@@ -140,11 +146,4 @@ def format_book_length(book):
     return f"{get_book_length(book).capitalize()}, {book.page_count} pages"
 
 
-# Formats the book length for HTML
-def format_category_for_search(category, selected_genre):
-    if selected_genre == "fiction":
-        formatted_category = "fiction+" + category
-    else:
-        formatted_category = category
-    return formatted_category
 

7eedae604dbf8c5f3cf0fc0c84aef64357ae0936
lucytesco <119863442+lucytesco@users.noreply.github.com>
2023-08-24 15:30:07 +0100

    Adds sendEmail() function for user to email the results

--- application/static/script.js
+++ application/static/script.js
@@ -51,5 +51,21 @@ function updateCategories() {
             }
         }
 
-        /* Initialize the categories based on the default genre selection*/
+        /* Initialize the categories based on the default genre selection */
         updateCategories();
+
+
+/* Function for user to email generated reading list to themselves */
+function sendEmail() {
+            var emailAddress = document.getElementById('email').value; /* Get the user's input to set as the email address */
+            var webpageURL = window.location.href; /* Set the link to send as the results page */
+            var emailSubject = "Your ChatpterOne Reading List"; /* Set the email subject to a default string */
+            var emailBody = webpageURL; /* Set the results page URL as the body of the email */
+            var mailtoLink = "mailto:" + emailAddress + "?subject=" + encodeURIComponent(emailSubject) + "&body=" + encodeURIComponent(emailBody); /* Create the mailto: link using the variables declared above using the encodeURIComponent() function to make sure the link is properly encoded */
+
+            /* Opens the user's default email client with a new message populated with the variables declared above */
+            window.location.href = mailtoLink;
+        }
+
+        /* Open the user's default email client to send the selected email recipient the results page URL */
+        sendEmail();
